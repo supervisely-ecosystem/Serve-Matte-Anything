21af9e2100da2344c1a26e392d91f5569967d0d5
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-12-16 19:25:30 +0400

    Update serve-matte-anything: supervisely 6.73.486, instance 6.15.35 (#5)

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/serve-matte-anything:1.0.2 . && \
-docker push supervisely/serve-matte-anything:1.0.2
+docker build -t supervisely/serve-matte-anything:1.0.3 . && \
+docker push supervisely/serve-matte-anything:1.0.3
